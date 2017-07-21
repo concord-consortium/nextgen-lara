776d9c9889e2a21d7d43a535fddcf08286549c20
Jason Simpson <jsimpson@concord.org>
2017-07-21 12:16:12 -0400

    Load json using ajax.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
 import { Link, Route } from 'react-router-dom'
 import './App.css';
-// how to request from local server http://localhost:3000/Interactions%20within%20the%20atmosphere_version_1.json
-import laraData from './data/interactions-within-the-atmosphere-version-1.json';
 import Pages    from './Pages.js';
 import Page     from './Page.js';
 
+var jsonUrl = 'http://localhost:3000/interactions-within-the-atmosphere-version-1.json';
+
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      laraData: null
+    }
+  }
+
+  loadLaraData() {
+
+    var _this = this;
+
+    fetch(jsonUrl)
+    .then(function(response) {
+      return Promise.resolve(response);
+    })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log("DATA", data);
+      console.log("_this", _this);
+      _this.setState({ laraData: data });
+    });
+
+  }
+
   render() {
+
+    var laraData = this.state.laraData;
+
+    console.log("render()", laraData);
+
+    if(laraData == null) {
+        return (
+            <button onClick={this.loadLaraData.bind(this)}>Load LARA data</button>
+        );
+    } 
+
     var pages = laraData.pages;
 
     console.log("LARA data", laraData);
@@ -26,7 +63,9 @@ class App extends Component {
         <Route path='/pages/' component={Page} />
       </div>
     );
+
   }
+
 }
 
 export default App;
