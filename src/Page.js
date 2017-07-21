2a4fe1e28748276c55ee794ce98a907c1a82c97b
Jason Simpson <jsimpson@concord.org>
2017-07-21 05:14:18 -0700

    Pass embeddables down to Page components.

--- src/Page.js
+++ src/Page.js
@@ -4,7 +4,25 @@ class Page extends Component {
   render() {
     var props = this.props.location.state;
     console.log("INFO Page", props);
-    return <span>Page {parseInt(props.page, 10) + 1} Content</span>;
+    return (
+        <div>
+          <span>Page {parseInt(props.page, 10) + 1} Content</span>
+          <ul>
+          { Object.keys(props.embeddables).map(function (e, index) {
+            return (
+              <li className="left" key={index}>  
+                <span className="padding">
+                      {props.embeddables[e].prompt} 
+                </span>
+                <span className="padding">
+                      {props.embeddables[e].type} 
+                </span>
+              </li>
+            ); 
+          }, this)}
+          </ul>
+        </div>
+      );
   }
 }
 
--- src/Pages.js
+++ src/Pages.js
@@ -12,14 +12,15 @@ class Pages extends Component {
     return (
 		<div className="pagesBar">
           { Object.keys(this.props.pages).map(function (page, index) {
-            // console.log('INFO Pages creating Link: ', page);
+            console.log('INFO Pages creating Link: ', this.props.pages[page]);
+            var embeddables = this.props.pages[page].embeddables;
             return (
                 <span key={"span-"+index} className="padding" >
 
                   <Link
                     to={{
                       pathname: "/pages/"+index,
-                      state: { page: page }
+                      state: { page: page, embeddables: embeddables }
                     }} 
                     key={index} >Page {index+1}</Link> 
 
